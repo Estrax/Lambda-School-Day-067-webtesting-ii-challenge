0879e8989df342d22aca8327b569dae532180441
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-09 18:03:48 -0400

    added the app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            strikes: 0,
+            balls: 0,
+        };
+
+        this.handleBall = this.handleBall.bind(this);
+        this.handleFoul = this.handleFoul.bind(this);
+        this.handleStrike = this.handleStrike.bind(this);
+        this.handleHit = this.handleHit.bind(this);
+        this.resetCounters = this.resetCounters.bind(this);
+    }
+
+    handleBall(){
+        if(this.state.balls === 3){
+            this.resetCounters();
+        }else{
+            this.setState({
+                ...this.state,
+                balls: (++this.state.balls)
+            })
+        }
+    }
+
+    handleFoul(){
+        this.setState({
+            ...this.state,
+            strikes: this.state.strikes === 2 ? 2 : ++this.state.strikes
+        });
+    }
+
+    handleStrike(){
+        if(this.state.strikes === 2){
+            this.resetCounters();
+        }else{
+            this.setState({
+                ...this.state,
+                strikes: ++this.state.strikes
+            })
+        }
+    }
+
+    handleHit(){
+        this.resetCounters();
+    }
+
+    resetCounters(){
+        this.setState({
+            strikes: 0,
+            balls: 0
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Display
+                    balls={this.state.balls}
+                    strikes={this.state.strikes}
+                />
+
+                <Dashboard
+                    handleBall={this.handleBall}
+                    handleFoul={this.handleFoul}
+                    handleStrike={this.handleStrike}
+                    handleHit={this.handleHit}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
